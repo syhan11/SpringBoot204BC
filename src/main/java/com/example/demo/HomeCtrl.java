3df9219fe3af2c6de7cd395e51bc203bcdd95a01
syhan11 <sueyoung.6311@gmail.com>
2019-07-15 14:39:14 -0400

    changed descriptions to display as a textarea with 3rows

--- src/main/java/com/example/demo/HomeCtrl.java
+++ src/main/java/com/example/demo/HomeCtrl.java
@@ -11,12 +11,15 @@ import javax.validation.Valid;
 @Controller
 public class HomeCtrl {
     @GetMapping("/tvform")
+    // passes empty tvshow object to "tvform"
     public String loadTvForm(Model model) {
         model.addAttribute("tvshow", new Tvshow());
         return "tvform";
     }
 
     @PostMapping("/tvform")
+    // passes validated tvshow object to tvshowconfirm
+    // otherwise it shoes error on tvform
     public String processTvForm(@Valid Tvshow tvshow, BindingResult result){
         if (result.hasErrors()) {
             return "tvform";
